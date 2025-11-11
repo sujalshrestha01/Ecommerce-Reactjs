d3a3fa9ee04050071f073ef0a7f4cfec7ad5ce68
Sujal Shrestha <sujal8696@gmail.com>
2025-11-11 22:08:05 +0545

    hash router

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import { Route, HashRouter  as Router, Routes } from "react-router-dom";
 import Sidebar from "./components/Sidebar";
 import MainContent from "./components/MainContent";
 import ProductPage from "./components/ProductPage";
--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/Ecommerce-Reactjs/'  // 
+  base: '/Ecommerce-Reactjs/'  
 })
